60b1232badce879fdf7b75e0c51076709cef4cd5
Freeman <eric.asante.freeman@gmail.com>
2025-11-12 00:59:11 +0000

    website name and entry field setup

--- main.py
+++ main.py
@@ -16,4 +16,11 @@ img = PhotoImage(file="logo.png")
 canvas.create_image(60, 120, image=img)
 canvas.grid(row=0, column=0, columnspan=2)
 
+# Website name label
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+# Label
+website_entry = Entry(width=50)
+website_entry.grid(row=1, column=1, padx=5, pady=5, ipady=2, columnspan=2, sticky="ew")
+
 window.mainloop()
