a89c5da996277f87ab3357373e8c8fa8eff9e565
Freeman <eric.asante.freeman@gmail.com>
2025-11-12 00:47:57 +0000

    window setup

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+from tkinter import *
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=30, pady=30)
+window.resizable(False, False)
