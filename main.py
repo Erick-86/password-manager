68c91cb0a20bec2e066e5abb7409a701a625a8fc
Freeman <eric.asante.freeman@gmail.com>
2025-11-12 00:54:36 +0000

    logo positioning

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=30, pady=30)
 window.resizable(False, False)
+
+# Logo 
+canvas = Canvas(width=150, height=170, highlightthickness=0)
+img = PhotoImage(file="logo.png")
+canvas.create_image(60, 120, image=img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+window.mainloop()
